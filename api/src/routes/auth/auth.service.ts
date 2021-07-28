3a29a88a0d209a81f04ea82233de2e325919667d
bojoluis21798 <bojoluis21798@gmail.com>
2021-07-28 17:39:08 +0800

    Create register user service

--- api/src/routes/auth/auth.service.ts
+++ api/src/routes/auth/auth.service.ts
@@ -1,15 +1,31 @@
-import { nanoid } from 'nanoid';
 import jwt from 'jsonwebtoken';
+import userModel from 'models/user.model';
+import CreateUserDto from './dto/create-user.dto';
+import bcrypt from 'bcrypt';
+import createHttpError from 'http-errors';
 
 const AuthService = {
-  createGuest: () => {
-    const userID = nanoid();
-    const token = jwt.sign(userID, 'jwt-secret');
+  /**
+   * Create User
+   * @param createUserDto
+   * @returns {string} JsonWebToken
+   */
+  createUser: async (createUserDto: CreateUserDto) => {
+    if (await userModel.findOne({ email: createUserDto.email })) {
+      throw new createHttpError.Conflict('User email already exists');
+    }
 
-    return {
-      userID,
-      token,
-    };
+    const salt = await bcrypt.genSalt(10);
+    createUserDto.password = await bcrypt.hash(createUserDto.password, salt);
+
+    const newUser = new userModel(createUserDto);
+    await newUser.save();
+
+    const { password, ...rest } = createUserDto;
+
+    const token = jwt.sign(rest, 'jwt-secret');
+
+    return token;
   },
 };
 
