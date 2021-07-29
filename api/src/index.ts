d18d80989aa788edc68a6d34a646f9de6e36fc8f
bojoluis21798 <bojoluis21798@gmail.com>
2021-07-29 16:34:48 +0800

    Install winston

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,12 +1,43 @@
 import 'reflect-metadata';
-import { createExpressServer } from 'routing-controllers';
+import express from 'express';
+import { useExpressServer } from 'routing-controllers';
 import connectDb from 'db/connectDb';
+import expressWinston from 'express-winston';
+import winston from 'winston';
 
 import AuthController from 'routes/auth/auth.controller';
 
-const app = createExpressServer({
+const app = express();
+
+app.use(
+  expressWinston.logger({
+    transports: [new winston.transports.Console()],
+    format: winston.format.combine(
+      winston.format.colorize(),
+      winston.format.json(),
+    ),
+    meta: false, // optional: control whether you want to log the meta data about the request (default to true)
+    msg: 'HTTP {{req.method}} {{req.url}}', // optional: customize the default logging message. E.g. "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}"
+    colorize: true, // Color the text and status code, using the Express/morgan color palette (text: gray, status: default green, 3XX cyan, 4XX yellow, 5XX red).
+    ignoreRoute: function (req, res) {
+      return false;
+    }, // optional: allows to skip some log messages based on request and/or response
+  }),
+);
+
+useExpressServer(app, {
   controllers: [AuthController],
 });
+
+app.use(
+  expressWinston.errorLogger({
+    transports: [new winston.transports.Console()],
+    format: winston.format.combine(
+      winston.format.colorize(),
+      winston.format.json(),
+    ),
+  }),
+);
 
 const PORT = process.env.PORT || 3000;
 
