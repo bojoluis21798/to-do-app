5fe6ae03764e5ae5d6cf3752378095bd0fa9cdd3
bojoluis21798 <bojoluis21798@gmail.com>
2021-07-29 16:21:01 +0800

    Refactor valid jwt middleware

--- api/src/middlewares/validJWT.middleware.ts
+++ api/src/middlewares/validJWT.middleware.ts
@@ -1,15 +1,18 @@
 import { NextFunction, Request, Response } from 'express';
 import createHttpError from 'http-errors';
 import jwt from 'jsonwebtoken';
+import { ExpressMiddlewareInterface } from 'routing-controllers';
 
-const validJWT = (req: Request, res: Response, next: NextFunction) => {
-  const token = req.header('Authorization')?.replace('Bearer ', '');
+class ValidJWT implements ExpressMiddlewareInterface {
+  use(req: Request, res: Response, next: NextFunction) {
+    const token = req.header('Authorization')?.replace('Bearer ', '');
 
-  if (token && jwt.verify(token, 'jwt-secret')) {
-    next();
-  } else {
-    throw new createHttpError.Unauthorized();
+    if (token && jwt.verify(token, 'jwt-secret')) {
+      next();
+    } else {
+      throw new createHttpError.Unauthorized();
+    }
   }
-};
+}
 
-export default validJWT;
+export default ValidJWT;
