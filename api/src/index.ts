1c5fa17cfda236a95583127ff55db07418c389a3
bojoluis21798 <bojoluis21798@gmail.com>
2021-07-29 16:21:14 +0800

    Use default error handler

--- api/src/index.ts
+++ api/src/index.ts
@@ -2,7 +2,6 @@ import 'reflect-metadata';
 import { createExpressServer } from 'routing-controllers';
 import express from 'express';
 import connectDb from 'db/connectDb';
-import errorHandler from 'middlewares/errorhandler.middleware';
 
 import AuthController from 'routes/auth/auth.controller';
 
@@ -15,8 +14,6 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 connectDb();
-
-app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`SERVER LISTENING AT PORT: ${PORT}`);
