54c0fc7dec19ba12be0306230c1f1ff952857e26
bojoluis21798 <bojoluis21798@gmail.com>
2021-07-31 16:41:18 +0800

    Create to do model

---
+++ api/src/models/todo.model.ts
@@ -0,0 +1,16 @@
+import { getModelForClass, prop, Ref } from '@typegoose/typegoose';
+import { Tags } from './tags.model';
+import { User } from './user.model';
+
+export class Todo {
+  @prop()
+  name: string;
+  @prop()
+  date: string;
+  @prop({ ref: () => Tags, type: () => String })
+  tag: Ref<Tags, string>[];
+  @prop({ ref: () => User, type: () => String })
+  created_by: Ref<User, string>;
+}
+
+export default getModelForClass(Todo);
