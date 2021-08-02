dfaa2feace2edb060f318c72d550b36a6f504ea0
bojoluis21798 <bojoluis21798@gmail.com>
2021-08-02 16:41:02 +0800

    Label error, error handler

--- api/src/middlewares/errorHandler.middleware.ts
+++ api/src/middlewares/errorHandler.middleware.ts
@@ -10,7 +10,7 @@ import { Service } from 'typedi';
 @Service()
 class ErrorHandler implements ExpressErrorMiddlewareInterface {
   error(error: any, request: Request, response: Response, next: NextFunction) {
-    console.log(error);
+    console.log('Error: ' + error);
     if (error instanceof HttpError) {
       response.status(error.statusCode).send(error);
     } else if ('httpCode' in error) {
