497dca3dbb4e62b152e084c6931d120647a5da4d
bojoluis21798 <bojoluis21798@gmail.com>
2021-08-02 14:14:11 +0800

    Handle possible token error in getBearer function

--- api/src/utils/getBearerToken.ts
+++ api/src/utils/getBearerToken.ts
@@ -1,7 +1,13 @@
 import { Request } from 'express';
 
 const getBearerToken = (req: Request) => {
-  return req.header('Authorization')?.replace('Bearer ', '');
+  const token = req.header('Authorization')?.replace('Bearer ', '');
+
+  if (token) {
+    return token;
+  } else {
+    throw new createHttpError.Unauthorized('No Token');
+  }
 };
 
 export default getBearerToken;
