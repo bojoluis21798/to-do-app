92c05baf46cc394cbaa4f97264eaf1881151b1c0
bojoluis21798 <bojolui21798@gmail.com>
2021-08-05 21:12:10 +0800

    Create register form

--- app/src/domain/register/Register.tsx
+++ app/src/domain/register/Register.tsx
@@ -1,17 +1,112 @@
 import React from 'react';
+import {Controller, useForm} from 'react-hook-form';
 import {Text, View} from 'react-native';
 import Button from '../../components/button/Button';
+import ErrorText from '../../components/errorText/ErrorText';
 import Input from '../../components/input/Input';
+import messages from '../../utils/messages';
+import patterns from '../../utils/patterns';
 import styles from './RegisterStyles';
 
+interface RegisterForm {
+  email: string;
+  password: string;
+  confirmPassword: string;
+}
+
 const Register: React.FunctionComponent = () => {
+  const {
+    control,
+    handleSubmit,
+    getValues,
+    formState: {isDirty, errors},
+  } = useForm<RegisterForm>();
+
+  const onSubmit = (data: RegisterForm) => console.log(data);
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Sign Up</Text>
-      <Input placeholder="Email Address" style={styles.input} />
-      <Input placeholder="Password" style={styles.input} />
-      <Input placeholder="Confirm Password" style={styles.input} />
-      <Button disabled style={styles.registerContainer}>
+      <Controller
+        control={control}
+        rules={{
+          required: {
+            value: true,
+            message: messages.isRequired,
+          },
+          pattern: {
+            value: patterns.email,
+            message: messages.emailInvalid,
+          },
+        }}
+        render={({field: {onChange, value}, fieldState: {invalid}}) => (
+          <Input
+            onChangeText={onChange}
+            value={value}
+            placeholder="Email Address"
+            style={styles.input}
+            hasErrors={invalid}
+          />
+        )}
+        name="email"
+        defaultValue=""
+      />
+      {errors.email && <ErrorText>{errors.email.message}</ErrorText>}
+      <Controller
+        control={control}
+        rules={{
+          required: {
+            value: true,
+            message: messages.isRequired,
+          },
+          pattern: {
+            value: patterns.password,
+            message: messages.invalidPassword,
+          },
+        }}
+        render={({field: {onChange, value}, fieldState: {invalid}}) => (
+          <Input
+            onChangeText={onChange}
+            value={value}
+            hasErrors={invalid}
+            secureTextEntry
+            placeholder="Password"
+            style={styles.input}
+          />
+        )}
+        name="password"
+        defaultValue=""
+      />
+      {errors.password && <ErrorText>{errors.password.message}</ErrorText>}
+      <Controller
+        control={control}
+        rules={{
+          required: {
+            value: true,
+            message: messages.isRequired,
+          },
+          validate: confirmPass => confirmPass === getValues('password'),
+        }}
+        render={({field: {onChange, value}, fieldState: {invalid}}) => (
+          <Input
+            onChangeText={onChange}
+            value={value}
+            secureTextEntry
+            hasErrors={invalid}
+            placeholder="Confirm Password"
+            style={styles.input}
+          />
+        )}
+        name="confirmPassword"
+      />
+      {errors.confirmPassword && (
+        <ErrorText>Confirm Password must match</ErrorText>
+      )}
+
+      <Button
+        onPress={handleSubmit(onSubmit)}
+        disabled={!isDirty}
+        style={styles.registerContainer}>
         Register
       </Button>
     </View>
