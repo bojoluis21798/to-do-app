ab02da691cc9f49c223a7688712cba5d03f94af8
bojoluis21798 <bojolui21798@gmail.com>
2021-08-05 21:12:01 +0800

    Create password mesage and pattern

--- app/src/utils/messages.ts
+++ app/src/utils/messages.ts
@@ -1,4 +1,6 @@
 export default {
   isRequired: 'This is required',
   emailInvalid: 'Invalid Email',
+  invalidPassword:
+    'Must be at least 8 characters, contains at least 1 uppercase letter, 1 lowercase letter, and 1 number',
 };
