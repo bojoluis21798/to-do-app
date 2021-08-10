da3e3c91d509693f83000acd8f90e25d3e6bcba8
bojoluis21798 <bojolui21798@gmail.com>
2021-08-10 16:22:07 +0800

    Create styled components theme

--- app/App.tsx
+++ app/App.tsx
@@ -6,18 +6,27 @@ import Routes from './src/routes/Routes';
 import authRoutes from './src/routes/authRoutes';
 import nonAuthRoutes from './src/routes/nonAuthRoutes';
 import {NativeRouter} from 'react-router-native';
+import {ThemeProvider} from 'styled-components/native';
+import {useColorScheme} from 'react-native';
+import LightTheme, {DarkTheme} from './src/theme/Theme';
 
 const App = () => {
   const {logIn} = store.getState();
+  const colorScheme = useColorScheme();
+
+  const currentTheme = colorScheme === 'dark' ? DarkTheme : LightTheme;
+
   return (
     <Provider store={store}>
-      <NativeRouter>
-        {logIn.loggedIn ? (
-          <Routes routes={authRoutes} />
-        ) : (
-          <Routes routes={nonAuthRoutes} />
-        )}
-      </NativeRouter>
+      <ThemeProvider theme={currentTheme}>
+        <NativeRouter>
+          {logIn.loggedIn ? (
+            <Routes routes={authRoutes} />
+          ) : (
+            <Routes routes={nonAuthRoutes} />
+          )}
+        </NativeRouter>
+      </ThemeProvider>
     </Provider>
   );
 };
