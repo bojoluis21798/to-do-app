77124a8b1dfb0a25387ba3499d3f3708bc4fcec4
bojoluis21798 <bojolui21798@gmail.com>
2021-08-13 18:12:17 +0800

    Convert to box

--- web/layouts/Home.tsx
+++ web/layouts/Home.tsx
@@ -1,4 +1,4 @@
-import { Flex, Text } from "@chakra-ui/react";
+import { Box, Flex, Text } from "@chakra-ui/react";
 import React from "react";
 
 const Home: React.FunctionComponent = ({ children }) => {
@@ -18,17 +18,15 @@ const Home: React.FunctionComponent = ({ children }) => {
       >
         <Text variant="title">Just a simple to do app</Text>
       </Flex>
-      <Flex alignItems="center" justifyContent="center" direction="column">
-        <Flex
-          flexDirection="column"
-          alignItems="center"
+      <Flex alignItems="center" justifyContent="center">
+        <Box
           backgroundColor="rgba(0,0,0,0.2)"
           p="5rem"
           w="30rem"
           borderRadius={10}
         >
           {children}
-        </Flex>
+        </Box>
       </Flex>
     </Flex>
   );
